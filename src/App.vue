35ea217c07d7ce1b80b790a836b74c5546e49501
kopyl <oleggggg1997@gmail.com>
2022-02-08 04:06:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <h3>Users:</h3>
+    <User :username=username v-for="{username} of users" :key='username'/>
+</template>
+
+<script>
+import User from './components/User.vue'
+
+export default {
+    name: 'App',
+
+    data() {
+        return {
+            users: [],
+            URLS: {
+                usersAPI: 'http://185.178.46.211:90/users'
+            }
+        }
+    },
+
+    beforeMount: function() {
+        this.getUsers()
+    },
+
+    methods: {
+
+        async getUsers() {
+            let users = await fetch(this.URLS.usersAPI)
+            this.users = await users.json()
+
+            // console.log('test2', process.env.NODE_ENV)
+            // console.log(presets)
+        }
+
+    },
+    
+    components: {
+        User
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+    margin-top: 60px;
+    margin-left: 2em
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,25 @@
+<template>
+    <a :href="`https://t.me/${this.username}`">
+    <h1>
+        <span>@ </span>{{ username }}
+    </h1>
+    </a>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  props: {
+    username: String
+  }
+}
+</script>
+
+<style scoped>
+    span {
+        opacity: .5;
+    }
+    a {
+        color: grey
+    }
+</style>
